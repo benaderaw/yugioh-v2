4f5ee6ad0aae8d9a2c64efcfcf011096f176e771
Ben Aderaw <asyncba@gmail.com>
2024-02-03 14:02:21 -0500

    added homepage and setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+
+export default function HomePage() {
+  return (
+    <>
+      <PageNav />
+    </>
+  );
+}
+
+// PAGE NAV
+// logo
+// search
+// navLinks
+
+// BODY
+// image
