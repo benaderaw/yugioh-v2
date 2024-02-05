51eef943c00aa25a6e61a731115a5af7c813369b
Ben Aderaw <asyncba@gmail.com>
2024-02-05 10:14:52 -0500

    added landing homepage content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import PageNav from "../components/PageNav";
+import yugioh from "../assets/yugioh.png";
+import styles from "./Homepage.module.css";
 
 export default function HomePage() {
   return (
     <div>
       <PageNav />
+
+      <div className={styles.landingContainer}>
+        <img src={yugioh} alt="" />
+        <div>
+          <span>Search</span>
+          <span>Collect</span>
+          <span>Win</span>
+        </div>
+      </div>
     </div>
   );
 }
-
-// PAGE NAV
-// logo
-// search
-// navLinks
-
-// BODY
-// image
